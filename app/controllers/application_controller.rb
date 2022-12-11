9b87aeee64d315ea70d17cff4321764ea08fab42
Duncan Miller <dmiller@heatspring.com>
2022-12-10 21:28:15 -0800

    trying different redirect for naked domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   def redirect_root_domain
     return unless request.host === "openshiro.com"
 
-    redirect_to("#{request.protocol}www.openshiro.com#{request.fullpath}", status: 301)
+    map = {"openshiro.com" => "www.openshiro.com"}
+    if (nh = map[request.host])
+      redirect_to request.url.gsub(request.host, nh), status: 301
+      return false
+    end
+
+    true
   end
 end

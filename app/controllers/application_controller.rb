ab49dd2bf5aa29bd755d42f0154af7c3a2f20930
Duncan Miller <dmiller@heatspring.com>
2022-12-10 22:32:33 -0800

    removing all redirects from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_root_domain
 
-  private
-
-  def redirect_root_domain
-    return unless request.host === "openshiro.com"
-
-    redirect_to("#{request.protocol}openshiro.com#{request.fullpath}", status: 301)
-  end
 end

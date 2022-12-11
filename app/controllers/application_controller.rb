1e732342aba7e77ade4487488eebdc1abe9279e7
Duncan Miller <dmiller@heatspring.com>
2022-12-10 21:20:36 -0800

    www redirect in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_root_domain
+
+  private
+
+  def redirect_root_domain
+    return unless request.host === 'openshiro.com'
+    redirect_to("#{request.protocol}www.openshiro.com#{request.fullpath}", status: 301)
+  end
+
 end

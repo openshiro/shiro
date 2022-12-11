edd11bb3702aaa7730b4510a46c5275420c2c386
Duncan Miller <dmiller@heatspring.com>
2022-12-10 21:23:48 -0800

    formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def redirect_root_domain
-    return unless request.host === 'openshiro.com'
+    return unless request.host === "openshiro.com"
+
     redirect_to("#{request.protocol}www.openshiro.com#{request.fullpath}", status: 301)
   end
-
 end
